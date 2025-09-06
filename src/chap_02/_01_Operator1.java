c64b1326bd8af65e799355f789b7b624bc124850
Dydiddl <dydiddl@naver.com>
2025-09-07 05:12:30 +0900

    2025-09-07

--- src/chap_02/_01_Operator1.java
+++ src/chap_02/_01_Operator1.java
@@ -46,6 +46,23 @@ public class _01_Operator1 {
         System.out.println(val++);
         System.out.println(val);
 
+        val = 10;
+        System.out.println(val);
+        System.out.println(--val);
+        System.out.println(val);
+
+        val = 10;
+        System.out.println(val);
+        System.out.println(val--);
+        System.out.println(val);
+
+        //  은행 대기번호 표
+        int waiting = 0;
+        System.out.println("대기인원 : " + waiting++);  //  대기인원 0, 변수++ 는 일단 현상환 그대로 코드를 진행하고, 그다음 +1 을 하는 것이다.
+        System.out.println("대기인원 : " + waiting++);  //  대기인원 1  따라서 ++변수 를 활용하면 +1을 한 후 현재 코드를 실행 한다는 의미이다.
+        System.out.println("대기인원 : " + waiting++);  //  대기인원 2
+        System.out.println("총 대기인원 : " + waiting);  //  대기인원 2
+
 
     }
 }
