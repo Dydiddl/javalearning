2ead8b76910b5cae06d202176464d7a59a88ae98
Dydiddl <dydiddl@naver.com>
2025-09-14 07:31:39 +0900

    2025-09-14 House commit

--- src/__01__ProgrammersQuiz/_01_IntroductoryQuestions/__AdditionOfFractions.java
+++ src/__01__ProgrammersQuiz/_01_IntroductoryQuestions/__AdditionOfFractions.java
@@ -1,13 +1,26 @@
 package __01__ProgrammersQuiz._01_IntroductoryQuestions;
 
 public class __AdditionOfFractions {
-//    public static int[] solution(int numer1, int denom1, int numer2, int denom2){
-//        int[] answer = {};
-//        return answer;
-//    }
+    public static int[] solution(int numer1, int denom1, int numer2, int denom2){
+        int[] answer = new int[2];
+        int numer = numer1 * denom2 + numer2 * denom1;
+        int denom = denom1 * denom2;
+        int commonMultiple = 1;
+
+        for (int i = 1; i <= numer && i <= denom; i++) {
+            if (numer % i == 0 && denom % i == 0) {
+                commonMultiple = i;
+            }
+        }
+
+        numer = numer / commonMultiple;
+        denom = denom / commonMultiple;
+        answer = new int[]{numer, denom};
+        return answer;
+    }
     public static void main(String[] args) {
-       double num1 = 1/2;
-       double num2 = 3/4;
-        System.out.println(num1);
+        int[] answer = solution(9, 2, 1, 3);
+        System.out.println(answer[0]);
+        System.out.println(answer[1]);
     }
 }
